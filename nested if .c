bb03b7cb75d01d7f6ec5657b8a1d55a4995e48b4
Sudharani9242 <162sudharani@gmail.com>
2024-11-26 00:38:58 -0800

    Update nested if .c

--- nested if .c
+++ nested if .c
@@ -12,16 +12,28 @@ int main() {
         printf("The number is positive.\n");
 
         
-        if (number % 2 == 0) {
+     
+ if (number % 2 == 0) {
             printf("The number is even.\n");
         } else {
             printf("The number is odd.\n");
         }
     }
-   
+    // If number is not positive, check if it's negative
     else if (number < 0) {
         printf("The number is negative.\n");
 
-       
+        // Nested if statement: Check if the negative number is even or odd
         if (number % 2 == 0) {
-            printf("
+            printf("The negative number is even.\n");
+        } else {
+            printf("The negative number is odd.\n");
+        }
+    }
+    // If number is zero
+    else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
