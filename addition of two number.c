1398ba2434d3df928c2429e48cafdc9fe6809420
Sudharani9242 <162sudharani@gmail.com>
2024-11-25 23:20:28 -0800

    Create addition of two number.c

---
+++ addition of two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int (number1,number2,result);
+scanf("%d",$number1);
+printf("enter the number1/n");
+scanf("%d",$number1);
+printf("enter the number2/n");
+scanf("%d",$number2);
+result=number1+number3);
+printf("%d +%d =%d,number1,number2,result");
+return 0;
+}
+
