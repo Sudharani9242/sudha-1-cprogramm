edc828d0287377ca0c116023668ad6847cc7ef64
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 05:03:40 +0530

    Create Area of circle.py

---
+++ Area of circle.py
@@ -0,0 +1,8 @@
+
+radius = float(input("Enter the radius of the circle: "))
+pi = 3.14159
+area = pi * (radius ** 2)
+print(f"The area of the circle with radius {radius} is {area}")
+output 
+enter the radius of circle :3.412
+the area of the circle eith radius 3.142 is 31.014291700759998
