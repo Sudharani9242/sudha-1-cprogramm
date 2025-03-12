bb8c139f58b980433d9ca8c0efde464253ebb134
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 05:24:48 +0530

    Create String manuplation.py

---
+++ String manuplation.py
@@ -0,0 +1,7 @@
+
+user_input = input("Enter a string: ")
+reversed_string = user_input[::-1]
+print("Reversed string:", reversed_string)
+otuput
+Enter a string: 1 2 3 4 5
+Reversed string: 5 4 3 2 1
