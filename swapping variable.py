1c1ff8057dc5a71e9fd2c6952557fd288e9abdd0
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 05:13:21 +0530

    Create swapping variable.py

---
+++ swapping variable.py
@@ -0,0 +1,13 @@
+
+a = 5
+b = 10
+
+print("Before swapping: a =", a, "b =", b)
+
+# Swap the values
+a = a + b  # a becomes 15 (5 + 10)
+b = a - b  # b becomes 5 (15 - 10)
+a = a - b  # a becomes 10 (15 - 5)
+
+print("After swapping: a =", a, "b =", b)
+
