c78a0243cc3f4995b4beed71803ad59df6a4d295
Sudharani9242 <162sudharani@gmail.com>
2025-03-12 20:45:28 +0530

    Create even or odd function.py

---
+++ even or odd function.py
@@ -0,0 +1,8 @@
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+num = 7
+result = check_even_or_odd(num)
+print(f"The number {num} is {result}.")
