fb5bd72ee4ef9419ec489a4d8b06cd663c94d543
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 15:21:24 +0530

    Create List sorting.py

---
+++ List sorting.py
@@ -0,0 +1,7 @@
+print("sudharani_27")
+numbers = [23, 5, 98, 12, 41, 73, 56, 3, 19, 61]
+ascending_order = sorted(numbers)
+descending_order = sorted(numbers, reverse=True)
+print("Original List:", numbers)
+print("Sorted in Ascending Order:", ascending_order)
+print("Sorted in Descending Order:", descending_order)
