cf792891c4b7dacb338e701dbe833f3e78f17651
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 05:42:56 +0530

    Create Even or odd using if else  ststmsent.py

---
+++ Even or odd using if else  ststmsent.py
@@ -0,0 +1,9 @@
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
+output
+Enter a number : 5
+5 is a odd number
