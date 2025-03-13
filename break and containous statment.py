b2317797a41a80f8109a795b2171f6f3e6625101
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 07:09:47 +0530

    Create break and containous statment.py

---
+++ break and containous statment.py
@@ -0,0 +1,10 @@
+
+for i in range(1, 11):
+    # If the number is 5, break out of the loop
+    if i == 5:
+        print("Breaking the loop at", i)
+        break
+ele if i % 2 == 0:
+        print("Skipping even number:", i)
+        continue
+    print("Odd number:", i)
