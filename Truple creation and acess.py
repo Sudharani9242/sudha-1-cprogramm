b8824453cd1665a8786080a18631a1a4b1574b3c
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 16:07:43 +0530

    Create Truple creation and acess.py

---
+++ Truple creation and acess.py
@@ -0,0 +1,9 @@
+print("sudharani_27")
+my_tuple = (1, 'apple', 3.14, True)
+
+int_value, str_value, float_value, bool_value = my_tuple
+
+print(f'Integer: {int_value}')
+print(f'String: {str_value}')
+print(f'Float: {float_value}')
+print(f'Boolean: {bool_value}')
