9c60b728c777cb6f7e4becd4694b3e044c40922c
Sudharani9242 <162sudharani@gmail.com>
2025-03-13 15:31:22 +0530

    Create List reversal.py

---
+++ List reversal.py
@@ -0,0 +1,6 @@
+print("sudharani_27")
+def reverse_list(input_list):
+    return input_list[::-1]
+my_list = [1, 2, 3, 4, 5]
+reversed_list = reverse_list(my_list)
+print("Reversed List:", reversed_list)
