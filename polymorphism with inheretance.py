03ca23ec128754d5c26d1a48ffc36151da9b1ce3
Sudharani9242 <162sudharani@gmail.com>
2025-03-14 20:42:03 +0530

    Create polymorphism with inheretance.py

---
+++ polymorphism with inheretance.py
@@ -0,0 +1,15 @@
+
+class Dog:
+    def speak(self):
+        return "Woof!"
+class Cat:
+    def speak(self):
+        return "Meow!"
+def animal_sound(animal):
+    print(animal.speak())
+dog = Dog()
+cat = Cat()
+
+
+animal_sound(dog)  
+animal_sound(cat)  
